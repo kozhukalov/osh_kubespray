84cd77459f168516058ab7fc239aefa83457e94b
Vladimir Kozhukalov <kozhukalov@gmail.com>
2024-08-01 08:09:18 -0500

    Use virt_type=qemu by default

--- scripts/deploy_openstack.sh
+++ scripts/deploy_openstack.sh
@@ -85,6 +85,8 @@ helm upgrade --install nova openstack-helm/nova \
     --namespace=openstack \
     --set bootstrap.wait_for_computes.enabled=true \
     --set conf.ceph.enabled=true \
+    --set conf.nova.libvirt.virt_type=qemu \
+    --set conf.nova.libvirt.cpu_mode=none \
     $(helm osh get-values-overrides -p ${OVERRIDES_DIR} -c nova ${FEATURES})
 
 PROVIDER_INTERFACE=provider1
